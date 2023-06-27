f26856f702c6cb59ad395640c289ef4b83a10dbe
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2023-06-27 18:04:55 -0500

    Cambio nombre de clases
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- projects/00-hola-mundo/src/TwitterFollowCard.jsx
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,20 +1,36 @@
-export function TwitterFollowCard({ children, userName, isFollowing }) {
+import { useState } from "react";
+
+export function TwitterFollowCard({ children, userName }) {
+  const [isFollowing, setIsFollowing] = useState(false);
+
   const text = isFollowing ? "Siguiendo" : "Seguir";
-  
+  const buttonClassName = isFollowing
+    ? "tw-followCard-button is-following"
+    : "tw-followCard-button";
+
+  const handleClick = () => {
+    setIsFollowing(!isFollowing);
+  };
 
   return (
-    <article>
-      <header>
-        <img src={`https://unavatar.io/twitter/${userName}`} alt={userName} />
+    <article className="tw-followCard">
+      <header className="tw-followCard-header">
+        <img
+          className="tw-followCard-avatar"
+          src={`https://unavatar.io/twitter/${userName}`}
+          alt={userName}
+        />
 
-        <div>
+        <div className="tw-followCard-info">
           <strong>{children}</strong>
-          <span>@{userName}</span>
+          <span className="tw-followCard-infoUserName">@{userName}</span>
         </div>
       </header>
 
       <aside>
-        <button>{text}</button>
+        <button className={buttonClassName} onClick={handleClick}>
+          {text}
+        </button>
       </aside>
     </article>
   );
--- projects/00-hola-mundo/src/main.jsx
+++ projects/00-hola-mundo/src/main.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import { App } from './App';
-import './index.css'
+import { App } from "./App";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -11,5 +11,5 @@ const Button = ({ text }) => {
 };
 
 root.render(
-  <App />
+  <App />,
 );
