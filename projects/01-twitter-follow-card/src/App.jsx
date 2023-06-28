b9e64079b41071f82dc6af9678047ffba6bf0baa
Frank Montalvo Ochoa <fgmo.dev@fmontalvoo.com>
2023-06-27 23:52:24 -0500

    Renderizado de listas
    Signed-off-by: Frank Montalvo Ochoa <fgmo.dev@fmontalvoo.com>

--- projects/01-twitter-follow-card/src/App.jsx
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -2,14 +2,32 @@ import "./App.css";
 import { TwitterFollowCard } from "./TwitterFollowCard";
 
 export function App() {
+  const users = [
+    {
+      uuid: crypto.randomUUID(),
+      name: "Frank Montalvo Ochoa",
+      userName: "fgmo_8a",
+      isFollowing: true,
+    },
+    {
+      uuid: crypto.randomUUID(),
+      name: "Miguel Ángel Durán",
+      userName: "midudev",
+      isFollowing: false,
+    },
+  ];
+
   return (
     <div className="App">
-      <TwitterFollowCard userName="fgmo_8a" initialIsFollowing>
-        Frank Montalvo Ochoa
-      </TwitterFollowCard>
-      <TwitterFollowCard userName="midudev">
-        Miguel Ángel Durán
-      </TwitterFollowCard>
+      {users.map(({ uuid, name, userName, isFollowing }) => (
+        <TwitterFollowCard
+          key={uuid}
+          userName={userName}
+          initialIsFollowing={isFollowing}
+        >
+          {name}
+        </TwitterFollowCard>
+      ))}
     </div>
   );
 }
