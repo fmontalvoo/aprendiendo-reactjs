d15001e3a97b347cb8a4593ea56a04940244d636
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2023-07-07 17:34:10 -0500

    Add standard lint
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
-import confetti from "canvas-confetti";
+import { useState } from 'react'
+import confetti from 'canvas-confetti'
 
 const TURNS = {
-  X: "X",
-  O: "O",
-};
+  X: 'X',
+  O: 'O'
+}
 
 const WINNER_COMBOS = [
   // Horizontales
@@ -17,94 +17,95 @@ const WINNER_COMBOS = [
   [2, 5, 8],
   // Diagonales
   [0, 4, 8],
-  [2, 4, 6],
-];
+  [2, 4, 6]
+]
 
+// eslint-disable-next-line react/prop-types
 const Square = ({ children, updateBoard, isSelected, index }) => {
-  const className = `square ${isSelected ? "is-selected" : ""}`;
+  const className = `square ${isSelected ? 'is-selected' : ''}`
 
   const handleClick = () => {
-    updateBoard(index);
-  };
+    updateBoard(index)
+  }
 
   return (
     <div onClick={handleClick} className={className}>
       {children}
     </div>
-  );
-};
+  )
+}
 
-function App() {
+function App () {
   const [board, setBoard] = useState(() => {
-    const boradFromStorage = localStorage.getItem("board");
+    const boradFromStorage = localStorage.getItem('board')
     return boradFromStorage
       ? JSON.parse(boradFromStorage)
-      : Array(9).fill(null);
-  });
+      : Array(9).fill(null)
+  })
 
   const [turn, setTurn] = useState(() => {
-    const turnFromStorage = localStorage.getItem("turn");
-    return turnFromStorage ?? TURNS.X;
-  });
+    const turnFromStorage = localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
 
   // `null` indica que no hay ganador y `false` que hay un empate
-  const [winner, setWinner] = useState(null);
+  const [winner, setWinner] = useState(null)
 
   const checkWinner = (boardToCheck) => {
     for (const combo of WINNER_COMBOS) {
-      const [a, b, c] = combo;
+      const [a, b, c] = combo
       if (
         boardToCheck[a] &&
         boardToCheck[a] === boardToCheck[b] &&
         boardToCheck[b] === boardToCheck[c]
       ) {
-        return boardToCheck[a];
+        return boardToCheck[a]
       }
     }
-    return null;
-  };
+    return null
+  }
 
   const checkEndGame = (boardToCheck) => {
-    return boardToCheck.every((square) => square !== null);
-  };
+    return boardToCheck.every((square) => square !== null)
+  }
 
   const resetGame = () => {
-    setBoard(Array(9).fill(null));
-    setTurn(TURNS.X);
-    setWinner(null);
-    localStorage.removeItem("turn");
-    localStorage.removeItem("board");
-  };
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+    localStorage.removeItem('turn')
+    localStorage.removeItem('board')
+  }
 
   const updateBoard = (index) => {
     // No actualizar si ya hay un valor
-    if (board[index] || winner) return;
+    if (board[index] || winner) return
 
     // Actualizar el tablero
-    const newBoard = [...board];
-    newBoard[index] = turn;
-    setBoard(newBoard);
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
 
     // Actualizar el turno
-    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
-    setTurn(newTurn);
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
 
-    localStorage.setItem("turn", newTurn);
-    localStorage.setItem("board", JSON.stringify(newBoard));
+    localStorage.setItem('turn', newTurn)
+    localStorage.setItem('board', JSON.stringify(newBoard))
 
-    const newWinner = checkWinner(newBoard);
+    const newWinner = checkWinner(newBoard)
     if (newWinner) {
-      confetti();
-      setWinner(newWinner); // La actualización de `winner` es asíncrona
+      confetti()
+      setWinner(newWinner) // La actualización de `winner` es asíncrona
     } else if (checkEndGame(newBoard)) {
-      setWinner(false);
+      setWinner(false)
     }
-  };
+  }
 
   return (
-    <main className="board">
+    <main className='board'>
       <h1>Tic tac toe</h1>
-      <section className="game">
+      <section className='game'>
         {board.map((square, index) => (
           <Square key={index} index={index} updateBoard={updateBoard}>
             {square}
@@ -112,19 +113,19 @@ function App() {
         ))}
       </section>
 
-      <section className="turn">
+      <section className='turn'>
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
 
       {winner !== null && (
-        <section className="winner">
-          <div className="text">
+        <section className='winner'>
+          <div className='text'>
             <h2>
-              {winner === false ? "Empate" : "Ganó:"}
+              {winner === false ? 'Empate' : 'Ganó:'}
             </h2>
 
-            <header className="win">
+            <header className='win'>
               {winner && <Square>{winner}</Square>}
             </header>
 
@@ -135,7 +136,7 @@ function App() {
         </section>
       )}
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
