b30986616dfa5db7b37e0f299b2adf51e9db835b
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2023-07-12 16:22:29 -0500

    Agrego funcion de seguimiento del mouse
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- projects/03-mouse-tracker/src/App.jsx
+++ projects/03-mouse-tracker/src/App.jsx
@@ -1,32 +1,60 @@
-/* eslint-disable */
 import { useEffect, useState } from "react";
 import "./App.css";
 
-function App() {
+const FollowMouse = () => {
   const [enabled, setEnabled] = useState(false);
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
-    console.log("useEffect");
+    console.log("useEffect", enabled);
+
+    const handleMouseMove = (e) => {
+      const { clientX, clientY } = e;
+      setCoords({ x: clientX, y: clientY });
+    };
+
+    if (enabled) {
+      window.addEventListener("pointermove", handleMouseMove);
+    }
+
+    return () => {
+      console.log("cleanup");
+      window.removeEventListener("pointermove", handleMouseMove);
+    }
+
   }, [enabled]);
 
   return (
     <main>
-      <div style={{
-        position: 'absolute',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-        border: '1px solid #fff',
-        borderRadius: '50%',
-        opacity: 0.8,
-        pointerEvents: 'none',
-        left: -25,
-        top: -25,
-        width: 50,
-        height: 50,
-        transform: `translate(${0}px, ${0}px)`
-      }} />
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+          border: "1px solid #fff",
+          borderRadius: "50%",
+          opacity: 0.8,
+          pointerEvents: "none",
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          transform: `translate(${coords.x}px, ${coords.y}px)`,
+        }}
+      />
       <button onClick={() => setEnabled(!enabled)}>
         {enabled ? "Desactivar" : "Activar"}
       </button>
+    </main>
+  );
+}
+
+function App() {
+  // const [mounted, setMounted] = useState(true);
+
+  return (
+    <main>
+      {/*mounted &&*/ <FollowMouse />}
+      {/*<button onClick={() => setMounted(!mounted)}>Toggle</button>*/}
     </main>
   );
 }
