10b3e5f79b47a1d3ff426ddb598b664dae9253db
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2023-07-20 16:13:12 -0500

    Completo prueba
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -1,5 +1,45 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+const API_CAT_FACTS = 'https://catfact.ninja/fact';
+const API_CAT_IMAGES = 'https://cataas.com';
+
 export function App() {
+
+    const [fact, setFact] = useState('');
+    const [image, setImage] = useState('');
+
+    useEffect(
+        () => {
+            fetch(API_CAT_FACTS)
+                .then(res => res.json())
+                .then(data => {
+                    const { fact } = data;
+
+                    setFact(fact);
+
+                    const firstWord = fact.split(' ', 1);
+
+                    fetch(`${API_CAT_IMAGES}/cat/says/${firstWord}?size=50&json=true`)
+                        .then(res => res.json())
+                        .then(data => {
+                            const { url } = data;
+
+                            setImage(url);
+                        });
+
+                });
+        },
+        []
+    );
+
     return (
-        <h1>Hello, world!</h1>
+        <main>
+            <h1>Random Cat Fact</h1>
+            {/* <section> */}
+            {fact && <p>{fact}</p>}
+            {image && <img src={`${API_CAT_IMAGES}${image}`} alt={`Image extracted using the fact: ${fact}`} />}
+            {/* </section> */}
+        </main>
     );
 }
