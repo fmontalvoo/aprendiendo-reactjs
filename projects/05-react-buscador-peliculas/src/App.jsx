70a2a6a5466a1790835857d7aca4e91d3bb2f2e5
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2023-08-02 11:28:28 -0500

    Manejo de errores en formulario de busqueda
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 // import { useRef } from 'react'
@@ -7,6 +8,8 @@ import { useMovies } from './hooks/useMovies'
 
 function App() {
   // const inputRef = useRef()
+  const [query, setQuery] = useState('')
+  const [error, setError] = useState(null)
 
   const { movies } = useMovies()
 
@@ -19,11 +22,29 @@ function App() {
     // const fields = new FormData(event.target)
     // const value = fields.get('query')
 
-    const fields = Object.fromEntries(new FormData(event.target))
-    const { query: value } = fields
+    // const fields = Object.fromEntries(new FormData(event.target))
+    // const { query: value } = fields
 
-    console.log(value)
+    console.log(query)
   }
+
+  const handleChange = (event) => {
+    const { value } = event.target
+    if (value.startsWith(' ')) return
+    setQuery(value)
+  }
+
+  useEffect(
+    () => {
+      if (query === '') {
+        setError('No hay un término de búsqueda')
+        return
+      }
+
+      setError(null)
+    },
+    [query]
+  )
 
   return (
     <div className='page'>
@@ -32,15 +53,17 @@ function App() {
       <header>
         <form className='form' onSubmit={handleSubmit}>
           {/* <input ref={inputRef} className='input' type="text" name='query' placeholder='Search...' /> */}
-          <input className='input' type="text" name='query' placeholder='Search...' />
+          <input value={query} onChange={handleChange} className='input' type="text" name='query' placeholder='Search...'
+            style={{ border: '1px solid transparent', borderColor: error ? 'red' : 'transparent' }} />
           <button type='sumbit'>Search</button>
         </form>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
       </header>
 
       <main>
         <Movies movies={movies} />
       </main>
-    </div>
+    </div >
   )
 }
 
