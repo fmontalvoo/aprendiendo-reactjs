bcf73040348a480d22847637a4a092553c43e66d
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2023-08-16 17:34:09 -0500

    Evitando la misma busqueda con useRef
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- projects/05-react-buscador-peliculas/src/hooks/useMovies.js
+++ projects/05-react-buscador-peliculas/src/hooks/useMovies.js
@@ -1,17 +1,21 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 import { searchMovies } from '../services/movies'
 
 export function useMovies({ search }) {
+    const previusSearch = useRef(search)
     const [movies, setMovies] = useState([])
 
     const [error, setError] = useState(null)
     const [loading, setLoading] = useState(false)
 
     const getMovies = async () => {
+        if (previusSearch.current === search) return
+
         try {
             setError(null)
             setLoading(true)
+            previusSearch.current = search
             const movies = await searchMovies({ search })
             setMovies(movies)
         } catch (e) {
